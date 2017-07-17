0df00044748c88483c3ac7ddc1f0dd5221ddec06
pat.guettler <patrick.j.guettler2.ctr@mail.mil>
2017-07-16 21:24:42 -0500

    Pulling account data, current eth price and account hashrate. Need to make things more dynamic. Also, add binding in for account number and other items.

--- cSharpApp/cSharpApp/ViewModels/NanoViewVm.cs
+++ cSharpApp/cSharpApp/ViewModels/NanoViewVm.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace cSharpApp.ViewModels
 {
     public class NanoViewVm
     {
+
         //TODO: Let the view set this...
         private string _ethUri = "https://api.etherscan.io/api?module=stats&action=ethprice&apikey=YourApiKeyToken";
+        private string _sixHourAvgHash = "https://api.nanopool.org/v1/eth/avghashratelimited/replaceMe/6";
+        private string _accountData = "https://api.nanopool.org/v1/eth/user/replaceMe";
+        
         public string EthUri
         {
             get
@@ -24,9 +30,23 @@ namespace cSharpApp.ViewModels
             }
         }
 
+        private string _accountNumber = string.Empty;
+        public string AccountNumber { get { return _accountNumber; } }
+
+        private string _ethUsd = string.Empty;
+        public string EthUsd { get { return _ethUsd; } }
+
+        private string _avgHash = string.Empty;
+        public string AvgHash { get { return _avgHash; } }
+
+        private string _accountBalance = string.Empty;
+        public string AccountBalance { get { return _accountBalance; } }
+
         public NanoViewVm()
         {
             GetCurrentEthPrice();
+            GetNanoPoolInfo();
+            GetAccountData();
         }
 
         public void GetCurrentEthPrice()
@@ -34,7 +54,60 @@ namespace cSharpApp.ViewModels
             using(var webClient = new WebClient())
             {
                 var json = webClient.DownloadString(_ethUri);
+                EthStuff ethStuff = JsonConvert.DeserializeObject<EthStuff>(json);
+                _ethUsd = "$" + ethStuff.result.ethusd;                
             }
         }
+
+        private void GetNanoPoolInfo()
+        {
+            using (var webClient = new WebClient())
+            {
+                var json = webClient.DownloadString(_sixHourAvgHash);
+                HashStuff hashStuff = JsonConvert.DeserializeObject<HashStuff>(json);
+                var sub = hashStuff.data.Remove(hashStuff.data.Length - 12);
+                _avgHash = sub + " MH/s";
+            }
+        }
+
+        private void GetAccountData()
+        {
+            using (var webClient = new WebClient())
+            {
+                var json = webClient.DownloadString(_accountData);
+                AccountStuff accountStuff = JsonConvert.DeserializeObject<AccountStuff>(json);
+                _accountBalance = accountStuff.data.balance;
+            }
+        }
+    }
+
+    internal class AccountStuff
+    {
+        public AccountData data {get; set;}
+    }
+
+    internal class AccountData
+    {
+        public string balance { get; set; }
+    }
+
+    internal class HashStuff
+    {
+        public string data { get; set; }
+    }
+
+    //TODO: Move this
+    internal class EthStuff
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+        public eth result { get; set; }
+    }
+
+    //TODO: Move this, too
+    internal class eth
+    {
+        public string ethbtc { get; set; }
+        public string ethusd { get; set; }
     }
 }
