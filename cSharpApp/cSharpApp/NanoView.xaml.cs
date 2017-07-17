2b9af9052d43ed548a228812bf2e2e38819857b6
pat.guettler <patrick.j.guettler2.ctr@mail.mil>
2017-07-16 19:42:56 -0500

    Working on setting up the vm

---
+++ cSharpApp/cSharpApp/NanoView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using cSharpApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cSharpApp
+{
+    /// <summary>
+    /// Interaction logic for NanoView.xaml
+    /// </summary>
+    public partial class NanoView : Window
+    {
+        NanoViewVm _vm;
+
+        public NanoView()
+        {
+            _vm = new NanoViewVm();
+            DataContext = _vm;
+            InitializeComponent();
+        }
+    }
+}
---
+++ cSharpApp/cSharpApp/ViewModels/NanoViewVm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpApp.ViewModels
+{
+    public class NanoViewVm
+    {
+        //TODO: Let the view set this...
+        private string _ethUri = "https://api.etherscan.io/api?module=stats&action=ethprice&apikey=YourApiKeyToken";
+        public string EthUri
+        {
+            get
+            {
+                return _ethUri;
+            }
+            set
+            {
+                _ethUri = value;
+                //Notify property changed.
+            }
+        }
+
+        public NanoViewVm()
+        {
+            GetCurrentEthPrice();
+        }
+
+        public void GetCurrentEthPrice()
+        {
+            using(var webClient = new WebClient())
+            {
+                var json = webClient.DownloadString(_ethUri);
+            }
+        }
+    }
+}
